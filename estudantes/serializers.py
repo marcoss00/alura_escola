b59ef6de7f3e738378caadaa313b89a485f9726a
marcoss00 <marcossvinc@gmail.com>
2025-08-18 18:28:35 -0300

    continuando na parte 2

--- estudantes/serializers.py
+++ estudantes/serializers.py
@@ -1,11 +1,21 @@
 from rest_framework import serializers
 from estudantes.models import Estudante, Curso, Matricula
+from estudantes.validators import cpf_invalido, nome_invalido, celular_invalido
 
 
 class EstudanteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Estudante
-        fields = ['id', 'nome', 'email', 'cpf', 'data_nascimento', 'celular']
+        fields = ['id','nome','email','cpf','data_nascimento','celular']
+
+    def validate(self,dados):
+        if cpf_invalido(dados['cpf']):
+            raise serializers.ValidationError({'cpf':'O CPF precisa ser válido!'})
+        if nome_invalido(dados['nome']):
+            raise serializers.ValidationError({'nome':'O nome só pode ter letras'})
+        if celular_invalido(dados['celular']):
+            raise serializers.ValidationError({'celular':'O celular precisa seguir o modelo: 86 99999-9999 (respeitando traços e espaços)'})
+        return dados
 
 
 class CursoSerializer(serializers.ModelSerializer):
